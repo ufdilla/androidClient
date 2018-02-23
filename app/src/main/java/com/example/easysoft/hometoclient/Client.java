2af21fb0785f9a162e874824d21733f5a59b1c14
ufdilla <learnmore.cumi@gmail.com>
2018-02-23 17:21:06 +0700

    tested

--- app/src/main/java/com/example/easysoft/hometoclient/Client.java
+++ app/src/main/java/com/example/easysoft/hometoclient/Client.java
@@ -2,33 +2,11 @@ package com.example.easysoft.hometoclient;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import android.util.Log;
-import android.view.SoundEffectConstants;
-import android.view.View;
-import android.widget.EditText;
-
-import org.json.JSONObject;
 
 public class Client extends Activity {
-
-    private Socket socket;
 
     private static final int SERVERPORT = 2003;
     private static final String SERVER_IP = "192.168.0.47";
@@ -37,7 +15,6 @@ public class Client extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.client);
-
     }
 
     public void onClick(View view) {
@@ -58,17 +35,13 @@ public class Client extends Activity {
         @Override
         public void run() {
 
-//                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
-//
-//                socket = new Socket(serverAddr, SERVERPORT);
-            String username = ((EditText) findViewById(R.id.username)).getText().toString();
-            String textRequest =((EditText) findViewById(R.id.textRequest)).getText().toString();
+        String username = ((EditText) findViewById(R.id.username)).getText().toString();
+        String textRequest =((EditText) findViewById(R.id.textRequest)).getText().toString();
+        String textDestination =((EditText) findViewById(R.id.textDestination)).getText().toString();
 
-//            JSONObject jObj = new JSONObject(textRequest);
-
-            TextView textViewResponse = (TextView) findViewById(R.id.textResponse);
-            Connector connector = new Connector(SERVER_IP, SERVERPORT, username, textRequest, textViewResponse);
-            connector.execute();
+        TextView textViewResponse = findViewById(R.id.textResponse);
+        Connector connector = new Connector(SERVER_IP, SERVERPORT, username, textRequest, textViewResponse, textDestination);
+        connector.execute();
 
         }
 
